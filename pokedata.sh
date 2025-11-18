06cec0c30e661b52cf514b8fddf07ef74382d44d
morganjg02 <morganjg02@gmail.com>
2025-11-18 17:22:08 -0600

    Cleaned up script to use awk instead of redundant recursive methods.

--- pokedata.sh
+++ pokedata.sh
@@ -5,15 +5,7 @@ FILE=pokedexScraper/pokedexData.csv
 read -p "Would you like to search for a specific Pokemon? (y/n): " search
 
 if [ "$search" == "n" ];then
-    # REWRITE BELLOW AS AWK SEARCH
-    while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4
-    do
-        echo "Displaying Dex Entry : $rec_column1"
-        echo "Name : $rec_column2"
-        echo "Type(s) : $rec_column3"
-        echo "Stat Total : $rec_column4"
-        echo ""
-    done < <(tail -n +2 $FILE)
+    awk -F ',' '{print "ID,NAME,TYPE,TOTAL"; print $1","$2","$3","$4}' $FILE | column -t -L -s ","
 else
     read -p "Enter Pokemon name: " name
     # if statement to check if pokemon name is in dex (case & spelling sensitive)
@@ -23,9 +15,12 @@ else
     # '$col ~ pokemon'             : regex to ensure that input name is in column 2
     # {found=1; print}             : set a variable (found) to true and print the line
     # | grep -q .                  : printed lines are piped to grep, which (-q) quietly
-    #                              : checks to see if awk printed anything
+    #                                checks to see if awk printed anything
     if awk -F ',' -v pokemon="$name" -v col=2 '$col ~ pokemon {found=1; print}' "$FILE" | grep -q .; then
-        awk -F ',' -v pokemon="$name" '$2 ~ pokemon {print $1,$2,$3,$4}' $FILE
+        {
+            echo "ID,NAME,TYPE,TOTAL"
+            awk -F ',' -v pokemon="$name" -v col=2 '$col ~ pokemon {print $1","$2","$3","$4}' $FILE
+        } | column -t -s ","
     else echo "Could not find '$name' in the Pokedex."
     fi
 fi
